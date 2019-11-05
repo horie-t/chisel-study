6da64b6e9b9f029b16e967f1ab2c706edfc3e0e0
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2019-11-06 08:01:47 +0900

    LCDディスプレイ: 作り直し

--- 050_LCDDisplay/src/main/scala/LCDDisplay.scala
+++ 050_LCDDisplay/src/main/scala/LCDDisplay.scala
@@ -1,116 +1,73 @@
 import chisel3._
 import chisel3.util._
 
-class LCDBundle extends Bundle {
+class LcdSpiBundle extends Bundle {
   val serialClock = Output(Bool())
   val dataCommand = Output(Bool())
-  val chipSelect = Output(Bool())
+  val chipSelectN = Output(Bool())
   val masterOutSlaveIn = Output(Bool())
   val masterInSlaveOut = Input(Bool())
 
-  val reset = Output(Bool())
+  val resetN = Output(Bool())
   val backLight = Output(Bool())
 }
 
-class OperationBundle extends Bundle {
-  val command = ValidIO(UInt(8.W))
-  val exec = Output(Bool())
-  val reset = Output(Bool())
-}
-
-class LCDDisplay extends Module {
-  val io = IO(new Bundle {
-    val lcd = new LCDBundle
-    val operation = Flipped(new OperationBundle)
-    val seg7LED = new Seg7LEDBundle
+class ILI9341SPI extends Module {
+  val io = IO(new Bundle{
+    val lcdSpi = new LcdSpiBundle
+    val sendData = Flipped(DecoupledIO(new Bundle{
+      val value = UInt(8.W)
+      val dataCommand = Bool()
+    }))
   })
 
-  // 周波数定義
-  val clockFrequency       = 100000000 // 100MHz
-  val serialClockFrequency =   5000000 //   5MHz
+  val spiClock = RegInit(true.B)
+  val chipSelect = RegInit(false.B)
+
+  val sendValue = RegInit(0.U(8.W))
+  val sendBitCount = RegInit(0.U(3.W))
+  val dataCommand = RegInit(false.B)
 
   // ステート定義
-  val (sIDLE :: sSEND :: Nil) = Enum(2)
-  val state = RegInit(sIDLE)
-  val execPulse = Debounce(io.operation.exec)
+  val (stateIdle :: stateSend :: Nil) = Enum(2)
+  val state = RegInit(stateIdle)
 
-  // クロックパルス生成
-  val (_, serialClockNegatePulse) = Counter(state =/= sIDLE, clockFrequency / serialClockFrequency / 2)
-  val serialClock = RegInit(true.B)
-  when (state === sIDLE && execPulse) {
-    serialClock := false.B
-  } .elsewhen (serialClockNegatePulse) {
-    serialClock := ~serialClock
-  }
-
-  // コマンドとパラメータ(3つまで)の一時メモリ
-  val initialCommandData = VecInit(Seq.fill(4){ 0.U(8.W) })
-  val commandData = RegInit(initialCommandData)
-  val commandDataWriteIndex = RegInit(0.U(2.W))
-  val commandDataSendIndex = RegInit(0.U(2.W))
-
-  /*
-   * データ入力
-   */
-  // スイッチからのコマンド、データの読み込み
-  when (Debounce(io.operation.command.valid)) {
-    commandData(commandDataWriteIndex) := io.operation.command.bits
-    commandDataWriteIndex := commandDataWriteIndex + 1.U
-  }
-
-  // コマンド、データのリセット
-  when (Debounce(io.operation.reset)) {
-    commandData := initialCommandData
-    commandDataWriteIndex := 0.U
-  }
-
-  /*
-   * コマンド送信
-   */
-  val stateChange = serialClock & serialClockNegatePulse
-
-  val sendShiftReg = Module(new ShiftRegisterPISO(8))
-  sendShiftReg.io.d := commandData(commandDataSendIndex)
-  sendShiftReg.io.load := false.B
-  sendShiftReg.io.enable := false.B
-  val (_, sendShiftWrap) = Counter(stateChange, 8)
-
-  when (state === sIDLE && execPulse) {
-    state := sSEND
-    sendShiftReg.io.load := true.B
-    commandDataSendIndex := commandDataSendIndex + 1.U
-  } .elsewhen (stateChange) {
-    when (sendShiftWrap) {
-      when (commandDataSendIndex === commandDataWriteIndex) {
-        state := sIDLE
-        commandData := initialCommandData
-        commandDataWriteIndex := 0.U
-        commandDataSendIndex := 0.U
-      } .otherwise {
-        sendShiftReg.io.load := true.B
-        commandDataSendIndex := commandDataSendIndex + 1.U
+  // 状態遷移
+  when(state === stateIdle && io.sendData.valid) {
+    state := stateSend
+    sendValue := Reverse(io.sendData.bits.value)
+    dataCommand := io.sendData.bits.dataCommand
+    spiClock := false.B
+    sendValue := 0.U
+  } .elsewhen(state === stateSend) {
+    spiClock := ~spiClock
+    when (spiClock) {
+      sendValue := Cat(sendValue(6, 0), 0.U(1.W))
+      sendBitCount := sendBitCount + 1.U
+      when (sendBitCount === 7.U) {
+        state := stateIdle
       }
-    } .otherwise {
-      sendShiftReg.io.enable := true.B
     }
   }
 
-  /*
-   * 出力
-   */
-  // コマンド、データをLEDに表示する
-  val seg7LED = Module(new Seg7LED)
-  seg7LED.io.digits := commandData.flatMap(x => Seq(x(7, 4), x(3, 0))).reverse
-  io.seg7LED := seg7LED.io.seg7led
 
-  io.lcd.chipSelect := state === sIDLE
-  io.lcd.serialClock := serialClock
-  io.lcd.dataCommand := commandDataSendIndex =/= 1.U
-  io.lcd.masterOutSlaveIn := sendShiftReg.io.shiftOut
-  io.lcd.reset := true.B
-  io.lcd.backLight := true.B
+  // 出力
+  io.lcdSpi.serialClock := spiClock & chipSelect
+  io.lcdSpi.dataCommand := dataCommand
+  io.lcdSpi.chipSelectN := state =/= stateSend
+  io.lcdSpi.masterOutSlaveIn := sendValue(7)
+  
+  io.lcdSpi.resetN := false.B
+  io.lcdSpi.backLight := true.B
+
+  io.sendData.ready := state === stateIdle
+}
+
+class LCDDisplay extends Module {
+  val io = IO(new Bundle{})
+
 }
 
 object LCDDisplay extends App {
-  chisel3.Driver.execute(args, () => new LCDDisplay)
+  Driver.execute(args, () => new LCDDisplay)
 }
