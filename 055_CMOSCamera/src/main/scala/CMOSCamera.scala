8c11b8702868ec310bd5532f3f35c5f8db5d8d13
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2019-11-16 21:41:50 +0900

    CMOSカメラ: I/Oを定義

---
+++ 055_CMOSCamera/src/main/scala/CMOSCamera.scala
@@ -0,0 +1,52 @@
+import chisel3._
+import chisel3.util._
+
+class LcdSpiBundle extends Bundle {
+  val serialClock = Output(Bool())
+  val dataCommand = Output(Bool())
+  val chipSelectN = Output(Bool())
+  val masterOutSlaveIn = Output(Bool())
+  val masterInSlaveOut = Input(Bool())
+
+  val resetN = Output(Bool())
+  val backLight = Output(Bool())
+}
+
+class CmosCameraBundle extends Bundle {
+  val systemClock = Output(Bool())       // カメラモジュールのシステムクロック(XCLK)
+  val verticalSync = Input(Bool())       // 垂直同期信号
+  val horizontalRef = Input(Bool())      // 水平基準線(trueの時にデータが有効)
+  val pixelclock = Input(Bool())         // ピクセルクロック
+  val pixcelData = Input(UInt(8.W))      // 画像データ(pixelCockの立ち上がりに読み込む)
+
+  val sccbClock = Output(Bool())         // SCCBのシリアルクロック
+  val sccbData = Output(Bool())          // SCCBのデータ
+
+  val resetN = Output(Bool())            // リセット
+  val powerDown = Output(Bool())         // 電源断
+}
+
+class CMOSCamera extends Module {
+  val io = IO(new Bundle{
+    val lcdSpi = new LcdSpiBundle
+    val cmosCam = new CmosCameraBundle
+  })
+
+  // 暫定出力
+  io.lcdSpi.serialClock := true.B
+  io.lcdSpi.dataCommand := true.B
+  io.lcdSpi.chipSelectN := true.B
+  io.lcdSpi.masterOutSlaveIn := true.B
+  io.lcdSpi.resetN := true.B
+  io.lcdSpi.backLight := true.B
+
+  io.cmosCam.systemClock := true.B
+  io.cmosCam.sccbClock := true.B
+  io.cmosCam.sccbData := true.B
+  io.cmosCam.resetN := true.B
+  io.cmosCam.powerDown := false.B
+}
+
+object CMOSCamera extends App {
+  Driver.execute(args, () => new CMOSCamera)
+}
